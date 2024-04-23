c18768276565a9f2bdb62678233a144a3bbedd51
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-04-23 16:25:21 +0200

    if changes applies, this commit will create new variables and initial README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # simple-app-js
- 
+
+# Project description
+
+This project develops a Pokédox as an application. This application presents lists about pokémon and the information about them.
+
+# Project dependencies
+
+- HTML
+- CSS
+- Javascript
--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-alert('Hello World');
-
-// Declaring variable
-let favouriteFood = 'Asian Food'
-document.write(favouriteFood);
+// Creating a new varibale
+let pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"] },
+  { name: "Caterpie", height: 0.3, types: ["Rock", "Fire"] },
+  { name: "Butterfree", height: 1.1, types: ["Ice", "Fly"] },
+  { name: "Pikachu", height: 0.4, types: ["Flying", "Electric"] },
+  { name: "Pidgey", height: 0.3, types: ["Rock", "Ground"] },
+];
