80e40adb1c3766c8f1e931c70bed4270f250d344
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-04-24 12:05:16 +0200

    adding styling, adding loop with conditional statement

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ let pokemonList = [
   { name: "Pikachu", height: 0.4, types: ["Flying", "Electric"] },
   { name: "Pidgey", height: 0.3, types: ["Rock", "Ground"] },
 ];
+// For loops to iterate over each item
+for (i = 0; i < pokemonList.length; i++) {
+  // print the message if height is above the certain value
+  if (pokemonList[i].height > 1.0) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        "-" +
+        "Wow, that’s big!" +
+        "<br/>"
+    );
+  } else {
+    // print the pokemaon name with their height
+    document.write(
+      pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br/>"
+    );
+  }
+}
