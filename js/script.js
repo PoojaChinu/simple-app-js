4431c64b4149c62dae4e657de777ec1817ac00c6
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-04-29 10:35:40 +0200

    Modifying for loop to ForEach loop and adding IIFE to pokemonList

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-// Creating a new varibale
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"] },
-  { name: "Caterpie", height: 0.3, types: ["Rock", "Fire"] },
-  { name: "Butterfree", height: 1.1, types: ["Ice", "Fly"] },
-  { name: "Pikachu", height: 0.4, types: ["Flying", "Electric"] },
-  { name: "Pidgey", height: 0.3, types: ["Rock", "Ground"] },
-];
-// For loops to iterate over each item
-for (i = 0; i < pokemonList.length; i++) {
+// creating new pokemonRepository variable and assign it to IIFE
+let pokemonRepository = (function () {
+  // Creating a new varibale
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"] },
+    { name: "Caterpie", height: 0.3, types: ["Rock", "Fire"] },
+    { name: "Butterfree", height: 1.1, types: ["Ice", "Fly"] },
+    { name: "Pikachu", height: 0.4, types: ["Flying", "Electric"] },
+    { name: "Pidgey", height: 0.3, types: ["Rock", "Ground"] },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+// add a new object to pokemon list
+pokemonRepository.add({
+  name: "Squirtle",
+  height: 0.5,
+  types: ["water", "ice"],
+});
+// using ForEach to iterate over each item
+pokemonRepository.getAll().forEach(function (pokemon) {
   // print the message if height is above the certain value
-  if (pokemonList[i].height > 1.0) {
+  if (pokemon.height > 1.0) {
     document.write(
-      pokemonList[i].name +
+      pokemon.name +
         " (height: " +
-        pokemonList[i].height +
+        pokemon.height +
         ")" +
         "-" +
         "Wow, that’s big!" +
@@ -22,7 +41,7 @@ for (i = 0; i < pokemonList.length; i++) {
   } else {
     // print the pokemaon name with their height
     document.write(
-      pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br/>"
+      pokemon.name + " (height: " + pokemon.height + ")" + "<br/>"
     );
   }
-}
+});
